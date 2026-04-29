250aaa5c2fb63c52f4460d86310d26bc7a0c1d8c
diegofg11 <diiegofernandezg@gmail.com>
2026-04-29 14:42:02 +0200

    FIX: Corrección de errores

--- app/src/main/java/com/diegofg11/pokequiz/ui/screens/MinigamesScreen.kt
+++ app/src/main/java/com/diegofg11/pokequiz/ui/screens/MinigamesScreen.kt
@@ -1,3 +1,7 @@
+package com.diegofg11.pokequiz.ui.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
