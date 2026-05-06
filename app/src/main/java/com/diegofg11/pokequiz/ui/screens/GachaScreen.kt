d1753cd61cad9230c3f7baeb013e9107fb3e5b83
diegofg11 <diiegofernandezg@gmail.com>
2026-05-06 11:11:17 +0200

    FEAT: Corrección de errores

--- app/src/main/java/com/diegofg11/pokequiz/ui/screens/GachaScreen.kt
+++ app/src/main/java/com/diegofg11/pokequiz/ui/screens/GachaScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.diegofg11.pokequiz.ui.theme.PokequizTheme
 import com.diegofg11.pokequiz.ui.components.PokemonAlertDialog
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
